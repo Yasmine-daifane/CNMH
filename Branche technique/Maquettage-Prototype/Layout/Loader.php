786785dec6985d4a50850f05bd9a9c9bb37b39c3
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2023-11-08 16:04:25 +0100

    push

---
+++ Branche technique/Maquettage-Prototype/Layout/Loader.php
@@ -0,0 +1,11 @@
+<?php
+define('Root', dirname(__FILE__));
+    error_reporting(E_ALL);
+
+    require_once Root . '.../../../DB/DataBaseConnection.php';
+    require_once Root . '.../../../DAO/CompetencesDAO.php';
+    require_once Root . '.../../../BLL/CompetencesBLL.php';
+    require_once Root . '.../../../Entities/Competence.php';
+
+
+    ?>
